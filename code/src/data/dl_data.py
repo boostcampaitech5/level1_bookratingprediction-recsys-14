0e2f7c204aa4a6c8863b902b958e3df6300ff45d
L2zz <ulla4571@gmail.com>
2023-04-17 12:35:01 +0000

    [Black] Apply black formatter to code/src/data
    Signed-off-by: L2zz <ulla4571@gmail.com>

--- code/src/data/dl_data.py
+++ code/src/data/dl_data.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader, Dataset
 
+
 def dl_data_load(args):
     """
     Parameters
@@ -16,46 +17,46 @@ def dl_data_load(args):
     """
 
     ######################## DATA LOAD
-    users = pd.read_csv(args.data_path + 'users.csv')
-    books = pd.read_csv(args.data_path + 'books.csv')
-    train = pd.read_csv(args.data_path + 'train_ratings.csv')
-    test = pd.read_csv(args.data_path + 'test_ratings.csv')
-    sub = pd.read_csv(args.data_path + 'sample_submission.csv')
+    users = pd.read_csv(args.data_path + "users.csv")
+    books = pd.read_csv(args.data_path + "books.csv")
+    train = pd.read_csv(args.data_path + "train_ratings.csv")
+    test = pd.read_csv(args.data_path + "test_ratings.csv")
+    sub = pd.read_csv(args.data_path + "sample_submission.csv")
 
-    ids = pd.concat([train['user_id'], sub['user_id']]).unique()
-    isbns = pd.concat([train['isbn'], sub['isbn']]).unique()
+    ids = pd.concat([train["user_id"], sub["user_id"]]).unique()
+    isbns = pd.concat([train["isbn"], sub["isbn"]]).unique()
 
-    idx2user = {idx:id for idx, id in enumerate(ids)}
-    idx2isbn = {idx:isbn for idx, isbn in enumerate(isbns)}
+    idx2user = {idx: id for idx, id in enumerate(ids)}
+    idx2isbn = {idx: isbn for idx, isbn in enumerate(isbns)}
 
-    user2idx = {id:idx for idx, id in idx2user.items()}
-    isbn2idx = {isbn:idx for idx, isbn in idx2isbn.items()}
+    user2idx = {id: idx for idx, id in idx2user.items()}
+    isbn2idx = {isbn: idx for idx, isbn in idx2isbn.items()}
 
-    train['user_id'] = train['user_id'].map(user2idx)
-    sub['user_id'] = sub['user_id'].map(user2idx)
-    test['user_id'] = test['user_id'].map(user2idx)
+    train["user_id"] = train["user_id"].map(user2idx)
+    sub["user_id"] = sub["user_id"].map(user2idx)
+    test["user_id"] = test["user_id"].map(user2idx)
 
-    train['isbn'] = train['isbn'].map(isbn2idx)
-    sub['isbn'] = sub['isbn'].map(isbn2idx)
-    test['isbn'] = test['isbn'].map(isbn2idx)
+    train["isbn"] = train["isbn"].map(isbn2idx)
+    sub["isbn"] = sub["isbn"].map(isbn2idx)
+    test["isbn"] = test["isbn"].map(isbn2idx)
 
     field_dims = np.array([len(user2idx), len(isbn2idx)], dtype=np.uint32)
 
     data = {
-            'train':train,
-            'test':test.drop(['rating'], axis=1),
-            'field_dims':field_dims,
-            'users':users,
-            'books':books,
-            'sub':sub,
-            'idx2user':idx2user,
-            'idx2isbn':idx2isbn,
-            'user2idx':user2idx,
-            'isbn2idx':isbn2idx,
-            }
-
+        "train": train,
+        "test": test.drop(["rating"], axis=1),
+        "field_dims": field_dims,
+        "users": users,
+        "books": books,
+        "sub": sub,
+        "idx2user": idx2user,
+        "idx2isbn": idx2isbn,
+        "user2idx": user2idx,
+        "isbn2idx": isbn2idx,
+    }
 
     return data
+
 
 def dl_data_split(args, data):
     """
@@ -70,14 +71,20 @@ def dl_data_split(args, data):
     """
 
     X_train, X_valid, y_train, y_valid = train_test_split(
-                                                        data['train'].drop(['rating'], axis=1),
-                                                        data['train']['rating'],
-                                                        test_size=args.test_size,
-                                                        random_state=args.seed,
-                                                        shuffle=True
-                                                        )
-    data['X_train'], data['X_valid'], data['y_train'], data['y_valid'] = X_train, X_valid, y_train, y_valid
+        data["train"].drop(["rating"], axis=1),
+        data["train"]["rating"],
+        test_size=args.test_size,
+        random_state=args.seed,
+        shuffle=True,
+    )
+    data["X_train"], data["X_valid"], data["y_train"], data["y_valid"] = (
+        X_train,
+        X_valid,
+        y_train,
+        y_valid,
+    )
     return data
+
 
 def dl_data_loader(args, data):
     """
@@ -91,14 +98,30 @@ def dl_data_loader(args, data):
     ----------
     """
 
-    train_dataset = TensorDataset(torch.LongTensor(data['X_train'].values), torch.LongTensor(data['y_train'].values))
-    valid_dataset = TensorDataset(torch.LongTensor(data['X_valid'].values), torch.LongTensor(data['y_valid'].values))
-    test_dataset = TensorDataset(torch.LongTensor(data['test'].values))
+    train_dataset = TensorDataset(
+        torch.LongTensor(data["X_train"].values),
+        torch.LongTensor(data["y_train"].values),
+    )
+    valid_dataset = TensorDataset(
+        torch.LongTensor(data["X_valid"].values),
+        torch.LongTensor(data["y_valid"].values),
+    )
+    test_dataset = TensorDataset(torch.LongTensor(data["test"].values))
 
-    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=args.data_shuffle)
-    valid_dataloader = DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=args.data_shuffle)
-    test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=args.data_shuffle
+    )
+    valid_dataloader = DataLoader(
+        valid_dataset, batch_size=args.batch_size, shuffle=args.data_shuffle
+    )
+    test_dataloader = DataLoader(
+        test_dataset, batch_size=args.batch_size, shuffle=False
+    )
 
-    data['train_dataloader'], data['valid_dataloader'], data['test_dataloader'] = train_dataloader, valid_dataloader, test_dataloader
+    data["train_dataloader"], data["valid_dataloader"], data["test_dataloader"] = (
+        train_dataloader,
+        valid_dataloader,
+        test_dataloader,
+    )
 
     return data
--- code/src/data/image_data.py
+++ code/src/data/image_data.py
@@ -9,6 +9,7 @@ from torch.autograd import Variable
 from tqdm import tqdm
 import os
 
+
 class Image_Dataset(Dataset):
     def __init__(self, user_isbn_vector, img_vector, label):
         """
@@ -25,14 +26,18 @@ class Image_Dataset(Dataset):
         self.user_isbn_vector = user_isbn_vector
         self.img_vector = img_vector
         self.label = label
+
     def __len__(self):
         return self.user_isbn_vector.shape[0]
+
     def __getitem__(self, i):
         return {
-                'user_isbn_vector' : torch.tensor(self.user_isbn_vector[i], dtype=torch.long),
-                'img_vector' : torch.tensor(self.img_vector[i], dtype=torch.float32),
-                'label' : torch.tensor(self.label[i], dtype=torch.float32),
-                }
+            "user_isbn_vector": torch.tensor(
+                self.user_isbn_vector[i], dtype=torch.long
+            ),
+            "img_vector": torch.tensor(self.img_vector[i], dtype=torch.float32),
+            "label": torch.tensor(self.label[i], dtype=torch.float32),
+        }
 
 
 def image_vector(path):
@@ -65,27 +70,27 @@ def process_img_data(df, books, user2idx, isbn2idx, train=False):
     ----------
     """
     books_ = books.copy()
-    books_['isbn'] = books_['isbn'].map(isbn2idx)
+    books_["isbn"] = books_["isbn"].map(isbn2idx)
 
     if train == True:
         df_ = df.copy()
     else:
         df_ = df.copy()
-        df_['user_id'] = df_['user_id'].map(user2idx)
-        df_['isbn'] = df_['isbn'].map(isbn2idx)
+        df_["user_id"] = df_["user_id"].map(user2idx)
+        df_["isbn"] = df_["isbn"].map(isbn2idx)
 
-    df_ = pd.merge(df_, books_[['isbn', 'img_path']], on='isbn', how='left')
-    df_['img_path'] = df_['img_path'].apply(lambda x: '/opt/ml/data/'+x)
-    img_vector_df = df_[['img_path']].drop_duplicates().reset_index(drop=True).copy()
+    df_ = pd.merge(df_, books_[["isbn", "img_path"]], on="isbn", how="left")
+    df_["img_path"] = df_["img_path"].apply(lambda x: "/opt/ml/data/" + x)
+    img_vector_df = df_[["img_path"]].drop_duplicates().reset_index(drop=True).copy()
     data_box = []
-    for idx, path in tqdm(enumerate(sorted(img_vector_df['img_path']))):
+    for idx, path in tqdm(enumerate(sorted(img_vector_df["img_path"]))):
         data = image_vector(path)
         if data.size()[0] == 3:
             data_box.append(np.array(data))
         else:
             data_box.append(np.array(data.expand(3, data.size()[1], data.size()[2])))
-    img_vector_df['img_vector'] = data_box
-    df_ = pd.merge(df_, img_vector_df, on='img_path', how='left')
+    img_vector_df["img_vector"] = data_box
+    df_ = pd.merge(df_, img_vector_df, on="img_path", how="left")
     return df_
 
 
@@ -104,43 +109,43 @@ def image_data_load(args):
             Batch size를 입력합니다.
     ----------
     """
-    users = pd.read_csv(args.data_path + 'users.csv')
-    books = pd.read_csv(args.data_path + 'books.csv')
-    train = pd.read_csv(args.data_path + 'train_ratings.csv')
-    test = pd.read_csv(args.data_path + 'test_ratings.csv')
-    sub = pd.read_csv(args.data_path + 'sample_submission.csv')
+    users = pd.read_csv(args.data_path + "users.csv")
+    books = pd.read_csv(args.data_path + "books.csv")
+    train = pd.read_csv(args.data_path + "train_ratings.csv")
+    test = pd.read_csv(args.data_path + "test_ratings.csv")
+    sub = pd.read_csv(args.data_path + "sample_submission.csv")
 
-    ids = pd.concat([train['user_id'], sub['user_id']]).unique()
-    isbns = pd.concat([train['isbn'], sub['isbn']]).unique()
+    ids = pd.concat([train["user_id"], sub["user_id"]]).unique()
+    isbns = pd.concat([train["isbn"], sub["isbn"]]).unique()
 
-    idx2user = {idx:id for idx, id in enumerate(ids)}
-    idx2isbn = {idx:isbn for idx, isbn in enumerate(isbns)}
+    idx2user = {idx: id for idx, id in enumerate(ids)}
+    idx2isbn = {idx: isbn for idx, isbn in enumerate(isbns)}
 
-    user2idx = {id:idx for idx, id in idx2user.items()}
-    isbn2idx = {isbn:idx for idx, isbn in idx2isbn.items()}
+    user2idx = {id: idx for idx, id in idx2user.items()}
+    isbn2idx = {isbn: idx for idx, isbn in idx2isbn.items()}
 
-    train['user_id'] = train['user_id'].map(user2idx)
-    sub['user_id'] = sub['user_id'].map(user2idx)
+    train["user_id"] = train["user_id"].map(user2idx)
+    sub["user_id"] = sub["user_id"].map(user2idx)
 
-    train['isbn'] = train['isbn'].map(isbn2idx)
-    sub['isbn'] = sub['isbn'].map(isbn2idx)
+    train["isbn"] = train["isbn"].map(isbn2idx)
+    sub["isbn"] = sub["isbn"].map(isbn2idx)
 
     img_train = process_img_data(train, books, user2idx, isbn2idx, train=True)
     img_test = process_img_data(test, books, user2idx, isbn2idx, train=False)
 
     data = {
-            'train':train,
-            'test':test,
-            'users':users,
-            'books':books,
-            'sub':sub,
-            'idx2user':idx2user,
-            'idx2isbn':idx2isbn,
-            'user2idx':user2idx,
-            'isbn2idx':isbn2idx,
-            'img_train':img_train,
-            'img_test':img_test,
-            }
+        "train": train,
+        "test": test,
+        "users": users,
+        "books": books,
+        "sub": sub,
+        "idx2user": idx2user,
+        "idx2isbn": idx2isbn,
+        "user2idx": user2idx,
+        "isbn2idx": isbn2idx,
+        "img_train": img_train,
+        "img_test": img_test,
+    }
     return data
 
 
@@ -158,13 +163,18 @@ def image_data_split(args, data):
     ----------
     """
     X_train, X_valid, y_train, y_valid = train_test_split(
-                                                        data['img_train'][['user_id', 'isbn', 'img_vector']],
-                                                        data['img_train']['rating'],
-                                                        test_size=args.test_size,
-                                                        random_state=args.seed,
-                                                        shuffle=True
-                                                        )
-    data['X_train'], data['X_valid'], data['y_train'], data['y_valid'] = X_train, X_valid, y_train, y_valid
+        data["img_train"][["user_id", "isbn", "img_vector"]],
+        data["img_train"]["rating"],
+        test_size=args.test_size,
+        random_state=args.seed,
+        shuffle=True,
+    )
+    data["X_train"], data["X_valid"], data["y_train"], data["y_valid"] = (
+        X_train,
+        X_valid,
+        y_train,
+        y_valid,
+    )
     return data
 
 
@@ -180,23 +190,33 @@ def image_data_loader(args, data):
     ----------
     """
     train_dataset = Image_Dataset(
-                                data['X_train'][['user_id', 'isbn']].values,
-                                data['X_train']['img_vector'].values,
-                                data['y_train'].values
-                                )
+        data["X_train"][["user_id", "isbn"]].values,
+        data["X_train"]["img_vector"].values,
+        data["y_train"].values,
+    )
     valid_dataset = Image_Dataset(
-                                data['X_valid'][['user_id', 'isbn']].values,
-                                data['X_valid']['img_vector'].values,
-                                data['y_valid'].values
-                                )
+        data["X_valid"][["user_id", "isbn"]].values,
+        data["X_valid"]["img_vector"].values,
+        data["y_valid"].values,
+    )
     test_dataset = Image_Dataset(
-                                data['img_test'][['user_id', 'isbn']].values,
-                                data['img_test']['img_vector'].values,
-                                data['img_test']['rating'].values
-                                )
+        data["img_test"][["user_id", "isbn"]].values,
+        data["img_test"]["img_vector"].values,
+        data["img_test"]["rating"].values,
+    )
 
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, num_workers=0, shuffle=True)
-    valid_dataloader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size, num_workers=0, shuffle=True)
-    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, num_workers=0, shuffle=False)
-    data['train_dataloader'], data['valid_dataloader'], data['test_dataloader'] = train_dataloader, valid_dataloader, test_dataloader
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.batch_size, num_workers=0, shuffle=True
+    )
+    valid_dataloader = torch.utils.data.DataLoader(
+        valid_dataset, batch_size=args.batch_size, num_workers=0, shuffle=True
+    )
+    test_dataloader = torch.utils.data.DataLoader(
+        test_dataset, batch_size=args.batch_size, num_workers=0, shuffle=False
+    )
+    data["train_dataloader"], data["valid_dataloader"], data["test_dataloader"] = (
+        train_dataloader,
+        valid_dataloader,
+        test_dataloader,
+    )
     return data
